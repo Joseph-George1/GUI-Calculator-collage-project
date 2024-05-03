0f5bec7d4f2ad050dc46c4668ac5e3fd722c206d
Joseph George <62404986+josephgeorge26@users.noreply.github.com>
2024-05-03 15:50:55 +0300

    Add files via upload

---
+++ Calculator_Source_Code/src/Calculator_Frame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame; 
+import javax.swing.WindowConstants;
+public class Calculator_Frame extends JFrame  {
+    public void init (){
+        setTitle("Welcome");
+        setSize(400,600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        
+        
+        
+    }
+    
+}
